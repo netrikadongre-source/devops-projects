47a4665cee8b718202f7e5416661efc4b7a1a2d5
Netrika Dongre <netrikadongre@gmail.com>
2025-08-23 02:15:56 +0530

    Delete Project-3-CICD-Docker-EC2 directory

--- Project-3-CICD-Docker-EC2/app/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.get("/", (_req, res) => {
-  res.send("🚀 Project-3: CI/CD with GitHub Actions → Docker Hub → EC2 (Docker)");
-});
-
-app.listen(port, () => console.log(`App running on port ${port}`));
